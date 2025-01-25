4110a6d4d44eb5701432d50f3af39b49da44a3de
Ali Elmi <ali.elmi77@yahoo.com>
2025-01-25 14:33:31 +0330

    add db and cache

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"log"
+
+	"github.com/alielmi98/golang-todo-list-api/config"
+	"github.com/alielmi98/golang-todo-list-api/constants"
+	"github.com/alielmi98/golang-todo-list-api/data/cache"
+	"github.com/alielmi98/golang-todo-list-api/data/db"
+)
+
+func main() {
+	cfg := config.GetConfig()
+
+	err := cache.InitRedis(cfg)
+	defer cache.CloseRedis()
+	if err != nil {
+		log.Fatalf("caller:%s  Level:%s  Msg:%s", constants.Redis, constants.Startup, err.Error())
+	}
+	err = db.InitDb(cfg)
+	defer db.CloseDb()
+	if err != nil {
+		log.Fatalf("caller:%s  Level:%s  Msg:%s", constants.Postgres, constants.Startup, err.Error())
+	}
+}
